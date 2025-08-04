cd8d528224aa9e1551e9f65e50c6311c90608059
MrEmon240 <mrimon1730@gmail.com>
2025-08-04 16:00:51 +0600

    problem five  don

--- problem.js
+++ problem.js
@@ -1,3 +1,5 @@
+// problem one 
+
 var area = 800;
 
 var property = area / 2;
@@ -6,7 +8,7 @@ console.log(property);
 
 
 
-
+// problem two 
 var money = 45000;
 if (money >= 45000) {
     console.log("Laptop");
@@ -17,3 +19,18 @@ else if (money >= 10000) {
 else{
     console.log("Chocolate");
 }
+
+
+
+
+// problem five 
+
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+
+
+var names =student.name;
+var rolls=student.roll;
+var departments =student.department;
+
+var email = names + rolls + "." +departments +  "@ph.ac.bd";
+console.log(email);
