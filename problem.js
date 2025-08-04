7eb339da501763d0cd26c1559d53bf621ecb84cd
MrEmon240 <mrimon1730@gmail.com>
2025-08-04 14:57:03 +0600

    problem two don

--- problem.js
+++ problem.js
@@ -2,3 +2,18 @@ var area = 800;
 
 var property = area / 2;
 console.log(property);
+
+
+
+
+
+var money = 45000;
+if (money >= 45000) {
+    console.log("Laptop");
+}
+else if (money >= 10000) {
+    console.log("Cycle");
+}
+else{
+    console.log("Chocolate");
+}
